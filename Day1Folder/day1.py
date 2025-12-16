129529b117a20e2ea0274097fcf7ffac2d14da71
winston12378 <winston_cahyadi@dovercourt.edu.sg>
2025-12-16 06:08:48 +0000

    Dec 16, 2025, 6:08 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -1,12 +1,18 @@
 # Write all your codes for Day 1 here.
 # COMMENT out the previous task before going on to the next task
-print("hello from day1")
-
+print("hello from day1 i am winston")
+print("this is second line")
+print("i am currently living in king albert park")
+print("my school is dover court international school")
+print("if i had 2000$ i would start investing")
+print("i am 14 years old")
 ########################################################################
 # Task 1:
-
-
-
+zoo1 = "tiger"
+zoo2 = "panda"
+zoo3 = "giraffe"
+zoo4 = "wolf"
+print( zoo2)
 ########################################################################
 # Task 2:
 
@@ -19,23 +25,51 @@ print("hello from day1")
 
 ########################################################################
 # Task 4:
-
-
+var1 = 20
+var2 = 26
+var3 = 10
+var4 = 2
+ans = var3 / var4
+ans2 = var1 + var2
+ans3 = var1 * var3
+print(ans)
+print (ans2)
+print (ans3)
 
 ########################################################################
 # Task 5:
-
+name = "dog"
+food = "bone"
 
 
 ########################################################################
 # Task 6:
-
+def greetings():
+    print("hello")
+    print("my name is winston")
+    print("i love to swim")
+greetings() # call a function
+greetings() # call a function
 
 
 ########################################################################
 # Task 7:
+def intro ("winston")
+print("nice to meet you winston")
+intro("goerge")
+intro("betty")
+print("winston")
 
 
 
 ########################################################################
-# Additional exercises:
+# Additional exercises:def teleport():
+    agent.teleport_to_player()
+
+    def tl():
+        agent.turn(left)
+player.on_chat("come", teleport)
+agent.move(forward, 1)
+agent.move(BACK,1)
+player.on_chat("tl", tl)
+def
